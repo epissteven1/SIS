2df81a8f354239455da52882cbd89f0d6769aac9
Steven <stevenepis9@gmail.com>
2025-01-28 13:38:41 +0800

    Initial commit

---
+++ src/test/java/com/example/InformationSystem/InformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InformationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
